db908b6be475288c39afd5e103594b4fb50461c9
Neil Betham <nbetham@me.com>
2014-10-15 13:42:59 -0400

    Initial Commit

---
+++ CollieClient/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  CollieClient
+//
+//  Created by Neil Betham on 10/15/14.
+//  Copyright (c) 2014 WMTU. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
