b4cf5ca155f826b73308616478c70edc85432758
Neil Betham <nbetham@me.com>
2014-10-20 02:08:32 -0400

    Initial Commit

---
+++ CollieClient/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CollieClient
+//
+//  Created by Neil Betham on 10/20/14.
+//  Copyright (c) 2014 WMTU. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
