1d72a0789af275c1bdc12d4fb1e023bee85df0c5
Neil Betham <nbetham@me.com>
2014-10-20 16:40:42 -0400

    Initial Commit

---
+++ CollieClient/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CollieClient
+//
+//  Created by Neil Betham on 10/20/14.
+//  Copyright (c) 2014 WMTU. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
